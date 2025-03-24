f40592915c3f92770fa74f3d76a83290ef453d20
EminAksuGH <nurullaheminaksu@hotmail.com>
2025-03-24 16:24:55 +0300

    Hosting changes

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
-﻿/* eslint-disable no-undef */
-import { defineConfig, loadEnv } from 'vite';
+﻿import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
-    process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
-
-    return {
-        plugins: [react()],
-        define: {
-            'import.meta.env': JSON.stringify(process.env),
-        }
-    };
+export default defineConfig({
+    plugins: [react()],
+    preview: {
+        host: '0.0.0.0',
+        port: 10000,
+        allowedHosts: [
+            'therapyai-production-4230.up.railway.app',
+            'therapyai-rm5c.onrender.com'
+        ],
+    },
 });
