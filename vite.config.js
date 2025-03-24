5675915b5132264fb7296e4b06ded04532ec7a86
EminAksuGH <nurullaheminaksu@hotmail.com>
2025-03-24 16:20:33 +0300

    Development deployment

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,13 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// Vite'in .env dosyasını otomatik yüklemesini sağlıyoruz
 export default defineConfig(({ mode }) => {
-    // Ortam değişkenlerini yükle
     process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
 
     return {
         plugins: [react()],
         define: {
-            'import.meta.env': JSON.stringify(process.env), // ✅ Doğru Çözüm
+            'import.meta.env': JSON.stringify(process.env),
         }
     };
 });
